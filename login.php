952f1233d1cb1b3b6fe74cbcfc465d163c25b009
lordblack <lordblackprogrammer@outlook.com>
2020-05-03 23:51:43 -0300

    mudanças no login e cadastro

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+$login=$_POST['login'];
+
+$entrar=$_POST['entrar'];
+
+senha=md5($_POST['senha']);
+
+$connect=mysql_connect('localhost','id13539777_equipemegahackdev','GJ+hy2@~8%?kla\G');
+
+$db=mysql_select_db('id13539777_programacion');
+
+if (isset($entrar)) {
+
+             
+
+      $verifica = mysql_query(“SELECT * FROM usuarios WHERE login = ‘$login’ AND senha = ‘$senha'”) or die(“erro ao selecionar”);
+
+        if (mysql_num_rows($verifica)<=0){
+
+          echo”<script language=’javascript’ type=’text/javascript’>alert(‘Login e/ou senha incorretos’);window.location.href=’login.html’;</script>”;
+
+          die();
+
+        }else{
+
+          setcookie(“login”,$login);
+
+          header(“Location:principal.html”);
+
+        }
+
+    }
+
+?>
